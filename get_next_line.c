c67bc15994d2a0191fb2bb7c3549f0f3931b7c41
Eduardo Castro <eduardocastrobritoo@gmail.com>
2023-06-15 13:26:42 -0300

    only save

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,156 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:14:51 by edcastro          #+#    #+#             */
-/*   Updated: 2023/06/14 21:46:43 by edcastro         ###   ########.fr       */
+/*   Updated: 2023/06/15 13:03:50 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_buffer_read(int fd, char *buffer)
+{
+	char	*temp;
+	int 	i;
+	
+	i = 0;
+	temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (temp == NULL)
+		return (NULL);
+	while (i)
+	{
+		i = read(fd, temp, BUFFER_SIZE);
+		if (i == -1 || i == 0)
+			break ;
+		temp[i] = '\0';
+		buffer = ft_strjoin(buffer, temp);
+		// if (buffer == NULL)
+		// {
+		// 	free(temp);
+		// 	return (NULL);
+		// }
+		if ((ft_strchr(buffer, '\n')))
+			break;
+	}
+	// free(temp);
+	// if (i == -1)
+	// 	return (NULL);
+	return (buffer);
+}
+
+static char	*ft_get_line(char *buffer)
+{
+	size_t	i;
+	char	*line;
+
+	line = NULL;
+	i = 0;
+	if (buffer[i] == '\0')
+		return (NULL);
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\0')
+		line = malloc((buffer[i] + 1) * sizeof(char));
+	else if (buffer[i] == '\n')
+		line = malloc((buffer[i] + 2) * sizeof(char));
+	if (line == NULL)
+		return (NULL);
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	return (line);
+}
+
+static char	*go_to_next_line(char *buffer)
+{
+	char	*next;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\0')
+	{
+		free(buffer);
+		return (NULL);
+	}
+	next = malloc((ft_strlen(buffer) - i + 1) * sizeof(char));
+	if (next == NULL)
+		return (NULL);
+	while ((buffer[i + j]) != '\0')
+	{
+		next[j] = buffer[i + j];
+		j++;
+	}
+	free(buffer);
+	return (next);
+}
+
 char	*get_next_line(int fd)
 {
-	return ;
+	static char	*buf;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = ft_buffer_read(fd, buf);
+	if (buf == NULL)
+		return (NULL);
+	line = ft_get_line(buf);
+	buf = go_to_next_line(buf);
+	return (line);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+int main(int argc, char *argv[])
+{
+    int fd;
+    char *line;
+    
+    if (argc != 2)
+        return (0);
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+        return (-1);
+
+
+    line = get_next_line(fd);
+    printf("fd: %s", line);
+    free(line);
+    
+    line = get_next_line(fd);
+    printf("fd: %s", line);
+    free(line);
+    
+    line = get_next_line(fd);
+    printf("fd: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("fd: %s", line);
+    free(line);
+
+    line = get_next_line(fd);
+    printf("fd: %s", line);
+    free(line);
+    //while (i <= 30)
+    // {
+        // printf("%d - %s", i, line);
+        //  free(line);
+        // if (line == NULL)
+            // printf("\n");
+        // line = get_next_line(fd);
+        // i++;
+    // }
+    close(fd);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,95 @@
 /*   By: edcastro <edcastro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 21:12:31 by edcastro          #+#    #+#             */
-/*   Updated: 2023/06/14 21:47:16 by edcastro         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:31:13 by edcastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (s == NULL)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *) s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *) s);
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while ((i + 1) < size && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (size)
+		dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	destiny;
+	size_t	source;
+	size_t	result;
+
+	if ((!dst && !src) || size == 0)
+		return (0);
+	i = 0;
+	result = 0;
+	destiny = ft_strlen(dst);
+	source = ft_strlen(src);
+	if (size > destiny)
+		result = source + destiny;
+	else
+		result = source + size;
+	while (size > (destiny + 1) && src[i] != '\0')
+	{
+		dst[destiny] = src[i];
+		destiny++;
+		i++;
+	}
+	dst[destiny] = '\0';
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_str = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (new_str != NULL)
+	{
+		ft_strlcpy(new_str, s1, s1_len + 1);
+		ft_strlcat(new_str, s2, s1_len + s2_len + 1);
+		return (new_str);
+	}
+	return (NULL);
+}
