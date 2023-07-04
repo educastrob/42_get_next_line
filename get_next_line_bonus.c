d1f567908d78393d59af5f7b3900142512b1de64
Eduardo Castro <95158753+educastrob@users.noreply.github.com>
2023-07-04 19:41:07 -0300

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -114,7 +114,7 @@ char	*get_next_line(int fd)
 	static char	*buffer[1024];
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd < 1024)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
 		return (NULL);
 	buffer[fd] = ft_buffer_read(fd, buffer[fd]);
 	if (!buffer[fd])
